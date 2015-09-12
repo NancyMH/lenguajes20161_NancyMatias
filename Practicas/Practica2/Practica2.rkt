0b84389e79a9d2484d4e5600b51024c0e7eeeec3
NancyMH <nancy_matias@ciencias.unam.mx>
2015-09-12 00:11:02 -0500

    Subiendo pruebas y comentarios de mapML

--- Practicas/Practica2/Practica2.rkt
+++ Practicas/Practica2/Practica2.rkt
@@ -34,14 +34,23 @@
 ;SECCION 2
 
 ;mapML
+;Recibira una función y una lista, la lista sera del tipo que definimos en las funciones anteriores, sera de tipo MList.
+;Si la lista que nos pasan es vacia, regresara vacio. En caso contrario definimos un MCons y le aplicamos la función a los
+;parametros de MCons.
+
 (define (mapML fun lst)
   (type-case MList lst
     [MEmpty () (MEmpty)]
     [MCons (num lst)
            (MCons (fun num) (mapML fun lst))]))
 
-(mapML add1 (MCons 7 (MCons 4 (MEmpty))))
-(mapML (lambda (x) (* x x)) (MCons 10 (MCons 3 (MEmpty))))
+;Testing
+(test (mapML add1 (MCons 7 (MCons 4 (MEmpty)))) (MCons 8 (MCons 5 (MEmpty))))
+(test (mapML (lambda (x) (* x x)) (MCons 10 (MCons 3 (MEmpty)))) (MCons 100 (MCons 9 (MEmpty))))
+(test (mapML sub1 (MCons 10 (MCons 8 (MCons 6 (MCons 4 (MEmpty)))))) (MCons 9 (MCons 7 (MCons 5 (MCons 3 (MEmpty))))))
+(test (mapML sub1 (MEmpty)) (MEmpty))
+(test (mapML (lambda(y) (* y 2)) (MCons 30 (MCons 20 (MCons 10 (MEmpty))))) (MCons 60 (MCons 40 (MCons 20 (MEmpty)))))
+
 
 ;lengthML
 (define (lengthML lst)
