eee4e452cf46ad9f270f1cd225d60cb45490630a
NancyMH <nancy_matias@ciencias.unam.mx>
2015-09-19 21:44:08 -0500

    Dudas en codigo y ejercicios

--- Practicas/Practica3/practica3.rkt
+++ Practicas/Practica3/practica3.rkt
@@ -1,3 +1,22 @@
 #lang plai
 
 (require "practica3-base.rkt")
+
+;;;No entiendo las formulas cual es el valor de i, 0.,4,?
+;;;Tampoco entiendo el problema, dado el minimo y el máximo hay que dar una zona?
+;(define (zone resting maximum)
+
+;Función auxiliar
+;(define (range rest max)
+ ; (cond
+  ;  [(zero? max) rest]
+   ; [(zero? rest) ((range*0.5)-1)]
+    
+  
+;Tampoco entiendo el problema, pense que teniamos que buscar una zona que nos pasaban como parametro dentro de una lista de zonas, pero eso no
+;tiene sentido, además por el ejmplo puesto en la practica creo que lo que estaba haciendo en el código de abajo esta mal.
+(define (get-zone name lst-zones)
+  (cond
+    [(empty? lst-zones) empty]
+    [(equal? name (car lst-zones)) car lst-zones]
+    [else (get-zone name (cdr lst-zones))]))
