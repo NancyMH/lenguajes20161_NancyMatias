096a97c01f6dc67c11344a74375b045ce0275c9b
NancyMH <nancy_matias@ciencias.unam.mx>
2015-11-08 00:24:05 -0600

    Correcciones

--- Practicas/Practica5/practica5.rkt
+++ Practicas/Practica5/practica5.rkt
@@ -5,17 +5,42 @@
   [num (n number?)]
   [bool (exp boolean?)]
   [Mlist (lst list?)                 
-         (car lst)
+         (car symbol?)
          (cdr list?)]
-  [with (bindings (listof bind?))    
+  [with (bindings (listof bind?))        
          (body RCFAEL?)]
   [rec (name-fun symbol?)           
        (params RCFAEL?)
        (body-fun RCFAEL?)]
   [fun (params RCFAEL?)              
        (body RCFAEL?)]
-;Faltan if y equals, no se me ocurrieron
-  [if (exp boolean?)])
+  [ifR (cond RCFAEL?)         
+       (res2 RCFAEL?)]
+  [equalR? (exp1 RCFAEL?)
+          (exp2 RCFAEL?)]
+  [op (exp procedure?)
+      (res RCFAEL?)]
+  [binop (exp procedure?)
+         (l RCFAEL?)
+         (r RCFAEL?)])
+
+
+
+(define-type Binding
+  [bind (name symbol?) (value RCFAEL?)])
+
+(define (operador op)
+  (case op
+    [(+) +]
+    [(-) -]
+    [(*) *]
+    [(/) /]
+    [(<) <]
+    [(>) >]
+    [(<=) <=]
+    [(>=) >=]
+    [(and) (lambda (x y) (and x y))]
+    [(or) (lambda (x y) (or x y))]))
 
 
 (define (desugar expr)
@@ -23,20 +48,34 @@
     [id (name) (id name)]
     [num (n) (num n)]
     [bool (exp) (bool exp)]
-    [Mlist lst
-           [(empty? lst) empty]
-           [cons (car lst)(cdr lst)]]
+    ;;[Mlist lst                                      ;;Marca un error que no entiendo
+      ;;     [(empty? lst) empty]
+        ;;   [cons (car lst)(cdr lst)]]
     [with (bindings body) 
            (app (fun (map (lambda(bind) 
                             (bind-name bind)) bindings)
                      (desugar body))
            (map (lambda (bind) 
                   (desugar (bind-val bind))) bindings))]
+    [with* (bindings body) (matryoshka bindings body)]
     [rec (name-fun params body-fun) (name-fun (- params  1)  (desugar body-fun))]
-    [fun(params body) (fun params (desugar body))]
-    [if (cond) (desugar #t)
-               (desugar #f)]
-    [equal? (exp1 exp2) 
-            [(equal? exp1 exp2) (bool #t)]
-            [(!equal? exp1 exp2) (bool #f)]]))
-    
+    [fun (params body) (fun params (desugar body))]
+    [ifR (cond then else) (ifR (desugar cond)               ;;No creo que esto este bien, no se me ocurre como hacerlo
+                         (bool #t)
+                         (bool #f))]
+    [equalR? (exp1 exp2) (equal?
+            [(equal? exp1 exp2) (desugar (bool #t))]
+            [(!equal? exp1 exp2) (desugar(bool #f))])]
+    [op (exp res) (op exp (desugar res))]
+    [binop (exp l r) (binop exp
+                            (desugar l)
+                            (desugar r))]))
+
+
+
+(define (matryoshka bindings body)
+  (cond
+    [(empty? bindings) (desugar body)]
+    [else (app (fun (list (bind-name (car bindings)))
+                    (matryoshka (cdr bindings) body))
+               (list (desugar (bind-val (car bindings)))))]))
