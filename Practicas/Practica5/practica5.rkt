113d4c7d8b0c5efd1cdcbb301347675927593e72
NancyMH <nancy_matias@ciencias.unam.mx>
2015-11-09 04:52:29 -0600

    practica 5 final

--- Practicas/Practica5/practica5.rkt
+++ Practicas/Practica5/practica5.rkt
@@ -16,7 +16,15 @@
   [equalR? (exp1 RCFAEL?)
         (exp2 RCFAEL?)]
   [op (f procedure?)
-   (value RCFAEL?)])
+      (value RCFAEL?)]
+  [ifR (arg RCFAEL?) 
+       (res1 RCFAEL?)
+       (res2 RCFAEL?)]
+  [rec (name-fun symbol?)           
+       (params RCFAEL?)
+       (body-fun RCFAEL?)])
+  ;[with (bindings (listof bind?))        
+   ;     (body RCFAEL?)]
 
 ;Declaración del tipo RCFAELS con sus respectivas variantes.
 ;Para cada caso se agregan los parámetros que se necesitarán en cada una de ellas.
@@ -32,7 +40,17 @@
   [equalRS? (exp1 RCFAELS?)
         (exp2 RCFAELS?)]
   [opS (f procedure?)
-       (value RCFAELS?)])
+       (value RCFAELS?)]
+  [ifRS (arg RCFAELS?) 
+       (res1 RCFAELS?)
+       (res2 RCFAELS?)]
+  [recS (name-fun symbol?)           
+        (params RCFAELS?)
+        (body-fun RCFAELS?)])
+;[withS (bindings (listof bind?))
+ ;        (body RCFAELS?)]
+  ;[with*S (bindings (listof bind?))
+
 
 
 ;Declaración de RCFAEL-Value, en donde simplemente agregamos el término
@@ -44,12 +62,18 @@
             (body RCFAEL?)
             (env Env?)])
 
+
 ;Se define el ambiente que se utilizará.
 (define-type Env
-  [mtSub]
-  [aSub (name symbol?) 
-        (value RCFAEL-Value?) 
-        (env Env?)])
+  [mtSub ]
+  [aSub (name symbol?)
+        (value RCFAEL-Value?)
+        (env Env?)]
+  [aRecSub (name symbol?)
+            (value boxed-RCFAEL-Value?)
+            (env Env?)])
+
+
 
 ; FUNCIONES AUXILIARES
 
@@ -64,6 +88,7 @@
     (if (or (boolean? bindRep) allow)
         (map (lambda (b) (bind (car b) (parse (cadr b)))) lst)
         (error 'parse-bindings (string-append "El id " (symbol->string (car bindRep)) " está repetido")))))
+
 
 ;Para las operaciones binarias, se recibe una operación dada una expresión y regresa
 ;el ya definido por Racket. 
@@ -80,15 +105,16 @@
     [(and) (lambda (x y) (and x y))]
     [(or) (lambda (x y) (or x y))]))
 
+
 ;Recibido un elemento, regresa la acción que ya está definida en 
 ;racket para llevar a cabo la operación correspondiente para operaciones unarias.
 (define (eligeUnario op)
   (case op
-    ;[(inc) inc]
-    ;[(dec) dec]
+    [(inc) add1]
+    [(dec) sub1]
     [(zero?) zero?]
     [(num?) number?]
-    [(neg) negative?]
+    [(neg) not]
     [(bool?) boolean?]
     [(first) first]
     [(rest) rest]
@@ -107,6 +133,7 @@
     [(member? (car l) (cdr l) comp) (car l)]
     [else (buscaRepetido (cdr l) comp)]))
 
+
 ;; member?: X listof(Y) (X Y -> boolean) -> boolean
 ;; Determina si x está en l usando "comparador" para
 ;; comparar x con cada elemento de la lista.
@@ -116,6 +143,7 @@
     [(empty? l) #f]
     [(comparador (car l) x) #t]
     [else (member? x (cdr l) comparador)]))
+
 
 ;; A::= <number>|<symbol>|listof(<A>)
 ;; parse: A -> RCFAEL
@@ -130,8 +158,21 @@
     [funS (params body) (fun params (desugar body))]
     [equalRS? (exp1 exp2)(equalR? (desugar exp1) (desugar exp2))]
     [opS (f value) (op f (desugar value))]
+    [ifRS (arg res1 res2) (ifR (desugar arg) 
+                               (desugar res1)
+                               (desugar res2))]
+    [recS (name-fun params body-fun) (rec (desugar name-fun)
+                                          (desugar params)
+                                          (desugar body-fun))]
+    ;[with (bindings body) 
+     ;      (app (fun (map (lambda(bind) 
+      ;                      (bind-name bind)) bindings)
+       ;              (desugar body))
+        ;   (map (lambda (bind) 
+         ;         (desugar (bind-val bind))) bindings))]
+    ;[with* (bindings body) (matryoshka bindings body)]
     ))
-
+ 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define-type Binding
@@ -156,6 +197,12 @@
                              (aSub params arg-val env))))]
     [equalR? (exp1 exp2)(auxeq (interp exp1 env) (interp exp2 env))]
     [op (f value) (auxUnario f (interp value env))]
+    [ifR (arg res1 res2) (bool (interp arg env)
+                               (interp res1 env)
+                               (interp res2 env))]
+    [rec (name-fun params body-fun) (rec (interp name-fun env)
+                                         (interp params env)
+                                         (interp body-fun env))]
     ))
 
 ;;Función auxiliar de op que lleva a cabo las operaciones unarias, recibe una opeación y el elemento a evaluar.
@@ -188,12 +235,33 @@
 
 ;Revisa si el id está dentro del ambiente. 
 (define (lookup name env)
- (type-case Env env
-   [mtSub () (error 'lookup "symbol is not in the env")]
-   [aSub (bound-name bound-value rest-env)
-         (if(symbol=? bound-name name)
-            bound-value
-            (lookup name rest-env))]))
+  (type-case Env env
+    [ mtSub () (error 'lookup "symbol is not in the env")]
+    [ aSub (bound-name bound-value rest-env)
+           (if (symbol=? bound-name name)
+               bound-value
+               (lookup name rest-env))]
+    [aRecSub (bound-name boxed-bound-value rest-env)
+              (if (symbol=? bound-name name)
+                  (unbox boxed-bound-value)
+                  (lookup name rest-env))]))
+
+
+;Definimos la caja con el valor
+(define (boxed-RCFAEL-Value? v)
+  (and (box? v)
+       (RCFAEL-Value? (unbox v))))
+
+
+
+;;cyclically-bind-and-interp : symbol RCFAE env → env
+(define (cyclically-bind-and-interp bound-id named-expr env)
+  (local ([define value-holder (box ( numV 1729 ))]
+          [define new-env ( aRecSub bound-id value-holder env)]
+          [define named-expr-val (interp named-expr new-env)])
+    (begin
+      (set-box! value-holder named-expr-val)
+      new-env)))
 
   
 (define (parse sexp)
@@ -208,6 +276,12 @@
        [(+ - / * < > <= >= and or) (binopS (elige (car sexp)) (parse (cadr sexp)) (parse (caddr sexp)))]
        [(inc dec zero? number? neg bool? first rest empty? list?) (opS (eligeUnario (car sexp)) (parse (cadr sexp)))]
        [(fun) (funS (cadr sexp) (parse (caddr sexp)))]
+       [(ifR) (ifRS (parse (car sexp)) 
+                   (parse (cadr sexp))
+                   (parse (caddr sexp)))]
+       [(rec) (recS (caadr sexp)
+                    (parse (cadadr sexp))
+                    (parse (caddr sexp)))]
        [else #t])]))
        ;[(with) (with (parse-bindings (cadr sexp) #f) (parse (caddr sexp)))]
        ;[(with*) (with*S (parse-bindings (cadr sexp) #t) (parse (caddr sexp)))]
@@ -222,5 +296,11 @@
 (test (rinterp (cparse 'false)) (boolV #f)) 
 (test (rinterp (cparse '{equal? 5 1})) (boolV #f))
 (test (rinterp (cparse '{equal? 10 10})) (boolV #t))
+(test (rinterp (if (>= 17 7) (num 17) (num 0))) (numV 17))
+(test (rinterp (if (=(* 3 7) 21) (num 1) (num 0))) (numV 1))
+(test (rinterp (if (= 4 8) (bool #t) (bool #f))) (boolV #f))
+(test (rinterp (cparse (if '{<= 15 5} 1 0))) (numV 1))
+(test (rinterp (cparse (if '{={/ 20 5} 4} 4 0))) (numV 4))
+
 
 
